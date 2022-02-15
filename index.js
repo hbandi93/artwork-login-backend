5c445d248ce2b58882d5990c0b2894a0a2e77a73
hbandi93 <hbandi93@gmail.com>
2022-02-15 15:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
