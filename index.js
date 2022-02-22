4b3455c4891409af693decc5c225f13b6e831ce7
hbandi93 <hbandi93@gmail.com>
2022-02-23 00:49:14 +0100

    Dummy for favs

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const port = 4000
 const cors = require('cors')
+const fs = require('fs')
+const data = require('./data.json')
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.post('/', (req, res) => {
+    data.push(req.body.url)
+    fs.writeFileSync('./data.json', JSON.stringify(data, null, 4))
+    res.sendStatus(200)
 })
 
 app.listen(port, () => {
