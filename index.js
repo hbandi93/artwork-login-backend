7b178882a09400a778546e81ba0a66ffc9995061
hbandi93 <hbandi93@gmail.com>
2022-02-23 19:12:35 +0100

    Dummy

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/', (req, res) => {
-    data.push(req.body.url)
+    data.push(req.body[0])
     fs.writeFileSync('./data.json', JSON.stringify(data, null, 4))
     res.sendStatus(200)
+})
+
+app.post('/profile', (req, res) => {
+    data.push(req.body.url)
+    fs.readFileSync('./data.json')
+    res.send(data[0])
 })
 
 app.listen(port, () => {
