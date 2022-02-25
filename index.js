e1d06f60e4f68602f8b87af40b25f2f9a1f9b7dd
hbandi93 <hbandi93@gmail.com>
2022-02-25 13:20:55 +0100

    For demo

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
 const process = require("process");
+const imageDetails = []
+const axios = require('axios')
 
 if (process.env.NODE_ENV !== "production") {
-  // Load environment variables from .env file in non prod environments
-  require("dotenv").config();
+    // Load environment variables from .env file in non prod environments
+    require("dotenv").config();
 }
 require("./utils/connectdb");
 
@@ -25,14 +27,14 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 //Add the client URL to the CORS policy
 
 const whitelist = process.env.WHITELISTED_DOMAINS
-  ? process.env.WHITELISTED_DOMAINS.split(",")
-  : [];
+    ? process.env.WHITELISTED_DOMAINS.split(",")
+    : [];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    callback(null, true);
-},
-  credentials: true,
+    origin: function (origin, callback) {
+        callback(null, true);
+    },
+    credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -42,13 +44,23 @@ app.use(passport.initialize());
 app.use("/users", userRouter);
 
 app.get("/", function (req, res) {
-  res.send({ status: "success" });
+    res.send({ status: "success" });
 });
+
+app.post("/images/Johnny", async (req, res) => {
+    imageDetails.push(req.body[0])
+    try {
+        const response = await axios.post("http://localhost:5000/", imageDetails)
+    } catch (e) {
+        console.log('error');
+    }
+    res.sendStatus(200)
+})
 
 //Start the server in port 8081
 
 const server = app.listen(process.env.PORT || 4000, function () {
-  const port = server.address().port;
+    const port = server.address().port;
 
-  console.log("App started at port:", port);
+    console.log("App started at port:", port);
 });
